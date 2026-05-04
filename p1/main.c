4142ffa762c344f8eca0bc947008ac655d05bab0
2025050156 <gecchopper273@gmail.com>
2026-05-04 15:07:48 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0, j = 0, k = 0;
+	bool found = false;
+
+	int A = 900, B = 750, C = 200;
+
+	int N; scanf("%d", &N);
+
+	for(i=1; i*A<=N; i++)
+		for(j=1; j*B<=N; j++)
+			for(k=1; k*C<=N; k++)
+				if(N - i * A - j * B - k * C == 0 && !(B % 2) && (k < i || k < j)) {
+					printf("%d %d %d\n", i, j, k);
+					found = true;
+				}
+
+	if(!found) printf("none");
+
+	return 0;
+}
